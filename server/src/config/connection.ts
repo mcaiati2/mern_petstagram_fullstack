f1decd4cd7004036de50ee5fd8b6ac2daac630a5
JD Tadlock <jt7903@gmail.com>
2024-11-21 13:05:53 -0500

    fixed connection

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://127.0.0.1:27017/pet_social_network_db');
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/pet_social_network_db');
 
 export default mongoose.connection;
