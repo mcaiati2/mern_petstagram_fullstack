5085903a88c343d34432e67f7834462eb7281810
JD Tadlock <jt7903@gmail.com>
2024-11-21 13:10:27 -0500

    fixed dotenv config code

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
 
-console.log(process.env.JWT_SECRET, process.env.MONGODB_URL);
-
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/pet_social_network_db');
 
 export default mongoose.connection;
