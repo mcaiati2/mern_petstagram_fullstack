5e79110b101d79836ccba05580c232cb91b6628d
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-11-25 14:05:14 -0500

    testing pull request merge

--- server/dist/server.js
+++ server/dist/server.js
@@ -10,7 +10,7 @@ import typeDefs from './schema/typeDefs.js';
 import resolvers from './schema/resolvers.js';
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3333;
+const PORT = Number(process.env.PORT) || 3333;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
