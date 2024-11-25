3bd66e7b0a406b555e41a0b6040113a0e33abda7
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-11-25 12:37:49 -0500

    testing pull request merge

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+
+const is_prod = process.env.PORT;
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:5173",
+    baseUrl: is_prod ? 'http://localhost:10000' : 'http://localhost:5173',
   },
 
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
